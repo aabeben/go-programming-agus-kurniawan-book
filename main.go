6af8dd3e8a403715a77da1d8bdd126b1fabdd02c
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-11 21:50:59 +0700

    comp demo.

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func main() {
-	fmt.Printf("%v\n", "Circle Area Calculation")
-	fmt.Print("Enter a radius value: ")
-	var radius float64
-	fmt.Scanf("%f", &radius)
-
-	area := math.Pi * math.Pow(radius, 2)
-	fmt.Printf("Circle area with radius %.2f = %.2f \n", radius, area)
+func main(){
+	var (
+		a = 2
+		b = 10
+	)
+	fmt.Println(a>b)
+	fmt.Println(a<b)
+	fmt.Println(a>=b)
+	fmt.Println(a<=b)
+	fmt.Println(a!=b)
+	fmt.Println(a==b)
 }
