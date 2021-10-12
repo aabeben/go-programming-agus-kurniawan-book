cd1afcc3e4383d919921d062fc59c47db8dfd4fd
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-12 10:35:42 +0700

    simple function.

--- main.go
+++ main.go
@@ -1,37 +1,13 @@
 package main
 
-
 import (
 	"fmt"
 	"math/rand"
 )
-
+func foo(){
+	fmt.Println("foo() was called!")
+	fmt.Println(rand.Intn(100))
+}
 func main(){
-	// define map
-	fmt.Println("define map")
-	products:=make(map[string]int)
-	customers:=make(map[string]int)
-
-
-	// insert data
-	fmt.Println(">>>>>insert into products map...")
-	products["product1"] = rand.Intn(100)
-	products["product2"] = rand.Intn(100)
-
-
-
-	// insert into customers map
-	fmt.Println(">>>>>insert into customers map...")
-	customers["customer1"] = rand.Intn(100)
-	customers["customer2"] = rand.Intn(100)
-
-	// display map data
-	fmt.Println(">>>>>display product map data...")
-	for name, value := range products {
-             fmt.Printf("%s => %d\n", name, value)
-	}
-	fmt.Println(">>>>display customers map data...")
-	for name, value:=range customers {
-		fmt.Printf("%s => %d\n", name, value)
-	}
+	foo()
 }
