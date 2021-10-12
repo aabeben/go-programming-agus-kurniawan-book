0ae577e19431fce09f3c92089762b0a0bcb14e5c
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-12 10:45:16 +0700

    function with returning value.

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
-package main 
+package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 
-func circle_area(r float64){
-	area := math.Pi * math.Pow(r, 2)
-	fmt.Printf("Circle area (r=%.2f) = %.2f \n",r, area)
-}
-func calculate(a, b, c float64){
-	result:=a*b*c
-	fmt.Printf("a=%.2f, b=%.2f, c=%.2f result=%.2f\n", a, b, c, result)
-}
 func main(){
-	var r float64 = 2.5
-	circle_area(r)
-	calculate(1.0, 2.0, 3.0)
+	a,b,c := 1.0, 2.0, 3.0
+	result:=advanced_calculate(a, b, c)
+	fmt.Printf("a = %.2f, b = %.2f, c = %.2f , result = %.2f\n",a, b, c, result)
+}
+func advanced_calculate(a, b, c float64) float64 {
+	result:=a*b*c
+	return result
 }
