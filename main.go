95482e5df1aad87c11a0831f11c1e0ac4e1d7c30
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-12 11:00:31 +0700

    function with multiple parameters and returning values.

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-
-func main(){
-	a,b,c := 1.0, 2.0, 3.0
-	result:=advanced_calculate(a, b, c)
-	fmt.Printf("a = %.2f, b = %.2f, c = %.2f , result = %.2f\n",a, b, c, result)
-}
-func advanced_calculate(a, b, c float64) float64 {
-	result:=a*b*c
+func add(numbers ...int) int{
+	result:=0
+	for _, number := range numbers{
+		result+=number
+	}
 	return result
+}
+func main(){
+	fmt.Printf("result = %d\n", add(1, 2, 3, 4, 5))
 }
