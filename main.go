25cf61c3199532da9b57987a8b000db4deb42d1c
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-12 09:50:00 +0700

    array demo.

--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	// iteration -for
-	var i int
-	for i = 0; i < 5; i++ {
-		if i == 3 {
-			fmt.Printf("i = %v\n\n", i)
-			break
-		}
-		fmt.Printf("i = %v\n", i)
+	// define array
+	fmt.Println("define array")
+	var numbers [5]int
+	var cities [5]string
+	var matrix [3][3]int // array 2D
+
+	// insert data
+	fmt.Println(">>>>>insert array data")
+	for i := 0; i < 5; i++ {
+		numbers[i] = i
+		cities[i] = fmt.Sprintf("City %d", i)
 	}
-	for j := 5; j < 11; j++ {
-		if j == 7 {
-			continue
+	// insert matrix data
+	fmt.Println(">>>>>>insert matrix data")
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			matrix[i][j] = rand.Intn(100)
 		}
-		fmt.Printf("j = %v\n", j)
+	}
+	// display data
+	fmt.Println(">>>>>>display array data")
+	for j := 0; j < 5; j++ {
+		fmt.Printf("number[%d] = %v\n", j, numbers[j])
+		fmt.Printf("cities[%d] = %v\n", j, cities[j])
+	}
+	// display matrix data
+	fmt.Println(">>>>>display matrix data")
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Printf("matrix[%d][%d] = %v\n", i, j, matrix[i][j])
+		}
 	}
 }
