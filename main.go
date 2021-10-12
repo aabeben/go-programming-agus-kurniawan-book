f60ed5eea2e63220e611942f9645d1dfda32263d
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-12 09:32:24 +0700

    Iteration - while.

--- main.go
+++ main.go
@@ -4,12 +4,11 @@ import "fmt"
 
 
 func main(){
-	// iteration - for 
-	var i int
-	for i=0; i< 5; i++ {
-		fmt.Printf("i = %v\n", i)
-	}
-	for j:=5; j<11; j++ {
-		fmt.Printf("j = %v\n", j)
+	// iteration - while
+	// go doesn't provide while syntax. we can use for
+	i:=0
+	for i<5{
+		fmt.Println(i)
+		i++
 	}
 }
