25fc97e4f5f2803bc892c190cf00813673be6b1b
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-12 09:39:15 +0700

    break and continue.

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import "fmt"
 
-
-func main(){
-	// iteration - while
-	// go doesn't provide while syntax. we can use for
-	i:=0
-	for i<5{
-		fmt.Println(i)
-		i++
+func main() {
+	// iteration -for
+	var i int
+	for i = 0; i < 5; i++ {
+		if i == 3 {
+			fmt.Printf("i = %v\n\n", i)
+			break
+		}
+		fmt.Printf("i = %v\n", i)
+	}
+	for j := 5; j < 11; j++ {
+		if j == 7 {
+			continue
+		}
+		fmt.Printf("j = %v\n", j)
 	}
 }
